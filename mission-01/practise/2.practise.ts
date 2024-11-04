de1373424195f693e5bf805fe9852a1da3324b18
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-11-04 16:07:19 +0600

    union and Intersection has been practise

--- mission-01/practise/2.practise.ts
+++ mission-01/practise/2.practise.ts
@@ -31,6 +31,47 @@
 
     console.log(user.city)
 
+    //type alias
+    type ID = number | string;
 
-    
+    type StudentInfo = {
+        name: string;
+        id: ID;
+        class:string,
+
+    }
+
+
+    const stdUserInfo: StudentInfo = {
+        name: 'omar',
+        id: 123,
+        class: 'seven'
+    }
+
+
+    //union and intersection
+
+    type treeColor = 'green' | 'yellow'
+
+
+    type UserPersonalInfo = {
+            name: string,
+            age?: number
+    }
+    type GeneralCredential = {
+        email: string,
+        passward: string | number,
+    }
+
+    type AuthenticInfo = UserPersonalInfo & GeneralCredential;
+
+
+    const permissionObj: AuthenticInfo = {
+        name: 'Isham',
+        email: 'omar@gmail.com',
+        passward: 123
+    }
+
+    console.log(permissionObj.email)
+
 }
